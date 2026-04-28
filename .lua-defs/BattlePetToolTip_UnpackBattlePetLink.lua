5e0c3203e5a9de17b9735ebea9ab36cd57402808
Shay Harding <3178267+kellewic@users.noreply.github.com>
2026-04-28 06:17:38 -0700

    Added new lua definitions for more accurate luals linting. Added new WoW API docs relevant to OneWoW for LLM use. Added new globals to luarc.

---
+++ .lua-defs/BattlePetToolTip_UnpackBattlePetLink.lua
@@ -0,0 +1,10 @@
+---@meta _
+
+---@return number? speciesID
+---@return number? level
+---@return number? breedQuality
+---@return number? maxHealth
+---@return number? power
+---@return number? speed
+---@return string? name
+function BattlePetToolTip_UnpackBattlePetLink(itemLink) end
--- .lua-defs/C_Item_ItemLocation_override.lua
+++ .lua-defs/C_Item_ItemLocation_override.lua
@@ -5,3 +5,9 @@ function C_Item.DoesItemExist(emptiableItemLocation) end
 
 ---@overload fun(itemLocation: ItemLocationMixin): string?
 function C_Item.GetItemLink(itemLocation) end
+
+---@overload fun(itemLocation: ItemLocationMixin): boolean
+function C_Item.CanBeRefunded(itemLocation) end
+
+---@overload fun(itemLocation: ItemLocationMixin): boolean
+function C_Item.CanScrapItem(itemLocation) end
